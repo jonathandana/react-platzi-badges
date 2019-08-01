ad2e97cbef4f192fb2e5d942316e81bf713b20ac
Jonathan Dana <jonathan@beeriprint.co.il>
2019-08-01 19:01:27 +0300

    add post in form

---
+++ src/components/Gravatar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import md5 from 'md5';
+
+function Gravatar(props){
+    const email = props.email;
+    const hash = md5(email);
+    const url = `https://www.gravatar.com/avatar/${hash}?d=identicon`;
+    return(
+        <img className={props.className} src={url} alt="Avatar" />
+    );
+}
+
+
+
+
+export default Gravatar;
