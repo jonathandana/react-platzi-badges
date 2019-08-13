42d11bc78e9afe86aaf796180df71a4cef76145c
Jonathan Dana <jonathan@beeriprint.co.il>
2019-08-13 17:26:20 +0300

    add delete badge

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,22 +4,22 @@ import ReactDOM from 'react-dom';
 import './styles/Modal.css';
 
 function Modal(props) {
-    if (!props.isOpen) {
-        return null;
-    }
+  if (!props.isOpen) {
+    return null;
+  }
 
-    return ReactDOM.createPortal(
-        <div className="Modal">
-            <div className="Modal__container">
-                <button onClick={props.onClose} className="Modal__close-button">
-                    X
-                </button>
+  return ReactDOM.createPortal(
+    <div className="Modal">
+      <div className="Modal__container">
+        <button onClick={props.onClose} className="Modal__close-button">
+          X
+        </button>
 
-                {props.children}
-            </div>
-        </div>,
-        document.getElementById('modal')
-    );
+        {props.children}
+      </div>
+    </div>,
+    document.getElementById('modal')
+  );
 }
 
 export default Modal;
